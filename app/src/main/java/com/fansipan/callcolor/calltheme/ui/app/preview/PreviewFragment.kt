5d08f9929f4bcdaea52c7b3598dbf01f1a0556ca
Phạm Trường <61191253+truongpa28@users.noreply.github.com>
2023-12-01 17:08:25 +0700

    push code 4.3

--- app/src/main/java/com/fansipan/callcolor/calltheme/ui/app/preview/PreviewFragment.kt
+++ app/src/main/java/com/fansipan/callcolor/calltheme/ui/app/preview/PreviewFragment.kt
@@ -4,9 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.bumptech.glide.Glide
+import com.fansipan.callcolor.calltheme.R
 import com.fansipan.callcolor.calltheme.base.BaseFragment
 import com.fansipan.callcolor.calltheme.databinding.FragmentPreviewBinding
+import com.fansipan.callcolor.calltheme.utils.SharePreferenceUtils
+import com.fansipan.callcolor.calltheme.utils.data.AvatarUtils
+import com.fansipan.callcolor.calltheme.utils.data.DataUtils
+import com.fansipan.callcolor.calltheme.utils.data.IconCallUtils
 import com.fansipan.callcolor.calltheme.utils.ex.clickSafe
+import com.fansipan.callcolor.calltheme.utils.ex.showOrGone
 
 class PreviewFragment : BaseFragment() {
 
@@ -28,7 +37,19 @@ class PreviewFragment : BaseFragment() {
     }
 
     private fun initView() {
-
+        DataUtils.callThemeEdit.let {item ->
+            Glide.with(requireContext())
+                .asBitmap()
+                .load(item.background)
+                .into(binding.imgBackground)
+            val posButton = item.buttonIndex.toInt() - 1
+            binding.imgIconCall1.setImageResource(IconCallUtils.listIconCall[posButton].icon1)
+            binding.imgIconCall2.setImageResource(IconCallUtils.listIconCall[posButton].icon2)
+            binding.imgAvatar.setImageResource(AvatarUtils.listAvatar[item.avatar.toInt()])
+            val loadAnimation: Animation = AnimationUtils.loadAnimation(requireContext(), R.anim.jump_button)
+            binding.imgIconCall1.startAnimation(loadAnimation)
+            binding.imgIconCall2.startAnimation(loadAnimation)
+        }
     }
 
     private fun initListener() {
